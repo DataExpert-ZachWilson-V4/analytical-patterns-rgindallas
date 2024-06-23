bf2fe7f6a39820290e5b5e3c7968df3ea4925ab2
rgindallas <richardgarth68@gmail.com>
2024-06-23 18:14:08 -0500

    Update query_5.sql

--- submission/query_5.sql
+++ submission/query_5.sql
@@ -1,19 +1,22 @@
---
--- Query 5: Which team has won the most games?
---
--- team_abbreviation	max_wins
--- SAS	                1182
---
+/*
 
-SELECT
-    team_abbreviation,
-    MAX(team_wins) AS max_wins
-FROM
-    rgindallas.nba_grouping_sets
-WHERE
-    aggregation_level = 'team'
-GROUP BY
-    team_abbreviation
-ORDER BY
-    max_wins DESC
+
+- Build additional queries on top of the results of the `GROUPING SETS` aggregations above to answer the following questions:
+  - Write a query (`query_3`) to answer: "Which player scored the most points playing for a single team?"
+  - Write a query (`query_4`) to answer: "Which player scored the most points in one season?"
+  - Write a query (`query_5`) to answer: "Which team has won the most games"
+
+*/
+
+-- query from the dashboard dataset created in Query 2
+SELECT team, won_games
+FROM harathi.nba_games_details_board
+WHERE aggregation_level = 'team'
+ORDER BY won_games DESC
 LIMIT 1
+
+/*
+Result : SAS (Spurs)
+team	won_games
+SAS	    14881
+*/
