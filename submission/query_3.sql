3cc9e65255878df828b941755c71ad10a8134c31
rgindallas <richardgarth68@gmail.com>
2024-06-23 18:12:34 -0500

    Update query_3.sql

--- submission/query_3.sql
+++ submission/query_3.sql
@@ -1,20 +1,20 @@
---
--- Which player scored the most points playing for a single team?
---
--- player_name	team_abbreviation	max_points
--- LeBron James	CLE	28314
---
-SELECT
-    player_name,
-    team_abbreviation,
-    MAX(total_points) AS max_points
-FROM
-    rgindallas.nba_grouping_sets
-WHERE
-    aggregation_level = 'player_team'
-GROUP BY
-    player_name,
-    team_abbreviation
-ORDER BY
-    max_points DESC
+/*
+- Build additional queries on top of the results of the `GROUPING SETS` aggregations above to answer the following questions:
+  - Write a query (`query_3`) to answer: "Which player scored the most points playing for a single team?"
+  - Write a query (`query_4`) to answer: "Which player scored the most points in one season?"
+  - Write a query (`query_5`) to answer: "Which team has won the most games"
+
+*/
+
+-- query from the dashboard dataset created in Query 2
+SELECT player, team, total_points
+FROM rgindallas.nba_games_details_board
+WHERE aggregation_level = 'player_and_team' 
+ORDER BY total_points DESC
 LIMIT 1
+
+/* 
+Result: LeBron James
+player	        team	total_points
+LeBron James	CLE	    28314 
+*/
