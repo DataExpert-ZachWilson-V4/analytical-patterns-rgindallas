e665bb86c46dfdf5ba6e76f58f69afc6091e51a9
rgindallas <richardgarth68@gmail.com>
2024-06-23 17:41:09 -0500

    Update query_3.sql

---
+++ submission/query_3.sql
@@ -0,0 +1,20 @@
+--
+-- Which player scored the most points playing for a single team?
+--
+-- player_name	team_abbreviation	max_points
+-- LeBron James	CLE	28314
+--
+SELECT
+    player_name,
+    team_abbreviation,
+    MAX(total_points) AS max_points
+FROM
+    jlcharbneau.nba_grouping_sets
+WHERE
+    aggregation_level = 'player_team'
+GROUP BY
+    player_name,
+    team_abbreviation
+ORDER BY
+    max_points DESC
+LIMIT 1
