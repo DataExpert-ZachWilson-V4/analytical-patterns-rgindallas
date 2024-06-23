95f1b9c7780a854a324ad185fb34252eef96bb77
rgindallas <richardgarth68@gmail.com>
2024-06-23 18:08:54 -0500

    Update query_1.sql

--- submission/query_1.sql
+++ submission/query_1.sql
@@ -1,41 +1,52 @@
-WITH yesterday AS (
-    SELECT *
-    FROM bootcamp.nba_players_state_tracking
-    WHERE current_season = 1996
-),
-today AS (
-    SELECT
-        player_name,
-        current_season,
-        MIN(s.season) AS first_active_season,
-        MAX(s.season) AS last_active_season
-    FROM bootcamp.nba_players,
-        UNNEST(seasons) s
-    WHERE current_season = 1997
-    GROUP BY 1, 2
-),
-combined AS (
-    SELECT
-        COALESCE (y.player_name, t.player_name) AS player_name,
-        COALESCE (y.first_active_season, t.first_active_season) AS first_active_season,
-        CASE
-            WHEN t.current_season - t.last_active_season != 0 THEN y.last_active_season
-            ELSE t.current_season
-        END AS last_active_season,
-        y.last_active_season AS season_yesterday,
-        t.current_season
-    FROM yesterday y
-    FULL OUTER JOIN today t
-        ON y.player_name = t.player_name
-)
-SELECT player_name,
+INSERT INTO rgindallas.nba_players_state_track
+WITH
+    yesterday AS (
+        SELECT
+            *
+        FROM
+            harathi.nba_players_state_track
+        WHERE
+            season = 1995 --parametrize
+    ),
+    today AS (
+        SELECT
+            player_name,
+            current_season
+        FROM
+            bootcamp.nba_players
+        WHERE
+            current_season = 1996 --parametrize
+            and is_active = true
+    ),
+    combined as (
+        SELECT
+            COALESCE(y.player_name, t.player_name) as player_name,
+            COALESCE(y.first_active_season, t.current_season) as first_active_season,
+            COALESCE(t.current_season, y.last_active_season) as last_active_season,
+            CASE
+                WHEN y.seasons_active IS NULL THEN ARRAY[t.current_season]
+                WHEN t.current_season IS NULL THEN y.seasons_active
+                ELSE y.seasons_active || ARRAY[t.current_season]
+            END as seasons_active,
+            y.last_active_season as active_previous_season,
+            t.current_season as active_season,
+            1996 as season -- parametrize
+        FROM
+            yesterday y
+            FULL OUTER JOIN today t ON y.player_name = t.player_name
+    )
+SELECT
+    player_name,
     first_active_season,
     last_active_season,
+    seasons_active,
     CASE
-        WHEN current_season - first_active_season = 0 THEN 'New'
-        WHEN last_active_season - season_yesterday = 0 THEN 'Continued Playing'
-        WHEN current_season - last_active_season = 1 THEN 'Retired'
-        WHEN current_season - season_yesterday > 1 AND current_season = last_active_season THEN 'Returned from Retirement'
-        WHEN current_season - last_active_season > 1 THEN 'Stayed Retired'
-    END AS season_active_state
-FROM combined
+        WHEN active_season - first_active_season = 0 THEN 'New'
+        WHEN active_season - active_previous_season > 1 THEN 'Returned from Retirement'
+        WHEN active_season - last_active_season = 0 THEN 'Continued Playing'
+        WHEN active_season IS NULL AND season - last_active_season = 1 THEN 'Retired'
+        ELSE 'Stayed Retired'
+    END as season_active_state,
+    season
+FROM
+    combined
