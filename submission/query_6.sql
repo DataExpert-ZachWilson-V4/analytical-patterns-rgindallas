ce3d31fef7acaf4f67755d3bf4aba59c25edd260
rgindallas <richardgarth68@gmail.com>
2024-06-23 18:42:03 -0500

    Update query_6.sql

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -1,23 +1,47 @@
--- Calculating cumulative wins and rolling 90-game wins for each team
-WITH team_game_results AS (
+/*
+Write a query (query_6) that uses window functions 
+on nba_game_details to answer the question: 
+"What is the most games a single team has won in a given 90-game stretch?"
+*/
+
+WITH nba_game_details_deduped AS (
+    SELECT 
+        *,
+        ROW_NUMBER() OVER (PARTITION BY game_id, team_id, player_id) row_number
+    FROM bootcamp.nba_game_details
+),
+
+combined AS (
     SELECT
-        ngd.team_id,
-        ngd.game_id,
-        ngd.team_abbreviation,
-        ng.game_date_est,
-        SUM(
-            CASE
-                WHEN ngd.team_id = ng.team_id_home THEN ng.home_team_wins
-                ELSE 1 - ng.home_team_wins
-            END
-        ) OVER (PARTITION BY ngd.team_id ORDER BY ng.game_date_est) AS cumulative_wins,
-        SUM(
-            CASE
-                WHEN ngd.team_id = ng.team_id_home THEN ng.home_team_wins
-                ELSE 1 - ng.home_team_wins
-            END
-        ) OVER (PARTITION BY ngd.team_id ORDER BY ng.game_date_est ROWS BETWEEN 89 PRECEDING AND CURRENT ROW) AS rolling_90_game_wins
-    FROM
-        nba_game_details_deduped ngd
-        JOIN nba_games_deduped ng ON ngd.game_id = ng.game_id
+        gd.game_id,
+        gd.team_id,
+        gd.team_abbreviation,
+        g.game_date_est,
+        MAX(CASE 
+            WHEN gd.team_id = g.home_team_id AND g.home_team_wins = 1 THEN 1   
+            WHEN gd.team_id = g.visitor_team_id AND g.home_team_wins = 0 THEN 1 
+            ELSE 0
+        END) AS team_won_game
+    FROM bootcamp.nba_games g 
+    JOIN nba_game_details_deduped gd ON g.game_id = gd.game_id AND gd.row_number = 1
+    GROUP BY 
+        gd.game_id, 
+        gd.team_id,
+        gd.team_abbreviation,
+        g.game_date_est
+    ),
+
+streaks AS (
+    SELECT *,
+        SUM(team_won_game) OVER (
+            PARTITION BY team_id ORDER BY game_date_est 
+            ROWS BETWEEN 89 PRECEDING AND CURRENT ROW
+        ) AS win_streak_90_games
+    FROM combined
 )
+
+SELECT team_abbreviation, MAX(win_streak_90_games) as max_games_won_90_day_stretch
+FROM streaks
+GROUP BY team_id, team_abbreviation
+ORDER BY max_games_won_90_day_stretch DESC
+LIMIT 1
