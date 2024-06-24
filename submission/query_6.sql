a3c51072e54a86a2ac2e41eea5cdd6606fdb40e2
rgindallas <richardgarth68@gmail.com>
2024-06-23 20:36:34 -0500

    Update query_6.sql

--- submission/query_6.sql
+++ submission/query_6.sql
@@ -2,6 +2,7 @@
 Write a query (query_6) that uses window functions 
 on nba_game_details to answer the question: 
 "What is the most games a single team has won in a given 90-game stretch?"
+
 */
 
 WITH nba_game_details_deduped AS (
